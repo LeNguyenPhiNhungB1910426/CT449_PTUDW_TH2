e609eeb4394fad3ec2097040598b3b1d22e5a5ea
LeNguyenPhiNhungB1910426 <nhungb1910426@student.ctu.edu.vn>
2022-09-22 13:49:28 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const contactsRouter = require("./app/routes/contact.route");
+
 const ApiError = require("./app/api-error");
 
 const app = express();
